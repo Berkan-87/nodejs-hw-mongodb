d2f5e072f3375c9afc3b40293a698abc388ffaca
Berkan-87 <sby.notess@gmail.com>
2025-09-10 00:09:59 +0300

    last

--- src/index.js
+++ src/index.js
@@ -10,8 +10,7 @@ async function bootstrap() {
 import dotenv from 'dotenv';
 dotenv.config();
 
-import initMongoConnection from './db/initMongoConnection.js';
-import { setupServer } from './server.js';
+
 
 const bootstrap = async () => {
   try {
